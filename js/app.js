a4029b23665e239825ce2383a9e2cba8e2573b7e
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-02-12 13:08:15 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+// Shaxsiy masala Start!
+function six(params) {
+  var tuglgan = prompt("Qachon tug'/lgansz") * 1;
+}
+
+// Shaxsiy masala end!
+
+// Birinchi masala Start !
+function one(params) {
+  var minut = prompt("Minut kiriting!") * 1;
+  var nat = minut * 60;
+  alert(minut + " Daqiqa " + nat + " soniya");
+}
+
+// Birinchi masala end !
+
+// Ikkinchi masala Start !
+function two(params) {
+  var yosh = prompt("Yoshni kiritng!") * 1;
+  var natkn = yosh * 365;
+  var natoy = natkn / 31;
+  var nathf = natkn / 7;
+  var natst = natkn * 24;
+  var natdq = natst * 60;
+  var natsn = natdq * 60;
+  alert(
+    "Siz " +
+      natoy +
+      " oy " +
+      nathf +
+      " hafta " +
+      natkn +
+      " kun " +
+      natst +
+      " soat " +
+      natdq +
+      " daqiqa " +
+      natsn +
+      " soniya umr korgansz!"
+  );
+}
+// Ikkinchi masala end !
+
+// Uchunchi masala start !
+function three(params) {
+  var soat = prompt("Soat kiriting!") * 1;
+  var nat = soat * 60;
+  alert(soat + " soat " + nat + " daqiqa");
+}
+
+// Uchunchi masala end !
+
+// tortinchi masala start !
+function four(params) {
+  var ism = prompt("Ismingizni kiriting!");
+  var fam = prompt("Familyangizni kiriting!");
+  alert(ism + " " + fam);
+}
+// tortinchi masala end !
+// tortinchi masala start !
+function five(params) {
+  var ism = prompt("Ismingizni kiriting!");
+  alert(" Assalomu alaykum " + ism + " aka");
+}
+// tortinchi masala end !
